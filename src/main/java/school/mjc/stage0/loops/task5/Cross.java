b06b64ff37784ccca1fce33423f622e24c960b12
Artak1999 <dashyan.1999@mail.ru>
2022-07-07 20:14:43 +0500

    Not hard.

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-        for (int i = 1; i < sideLength; i++) {
-            for (int j = 1; j < sideLength; j++) {
-                if(i == 4 || j == 4)
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if(i == sideLength/2 || j == sideLength/2)
                     System.out.print("*");
                 else
                     System.out.print(" ");
