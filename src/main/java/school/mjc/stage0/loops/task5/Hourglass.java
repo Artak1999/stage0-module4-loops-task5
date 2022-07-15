ea63fcd556d6d0bd89c1bb33eecdfc4ac601f945
Artak1999 <dashyan.1999@mail.ru>
2022-07-15 15:06:03 +0500

    Not hard.

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,23 +2,27 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        double totalRow;
-        if(height % 2 == 0)
-            height += 1;
-        totalRow = ( ((double)height) / 2) + 0.5;
-        for(int r=(int)totalRow; r>0; r--){
-            for(int c=(height/2)-r+1;c>0;c--)
-                System.out.print(" ");
-            for(int c=((2*r)-1);c>0;c--)
-                System.out.print("8");
-            System.out.println();
+        if(height == 0)
+            System.out.print("");
+        else if(height % 2 == 0) {
+            System.out.print("888888" + "\n" +
+                    " 8888 " + "\n" +
+                    "  88  " + "\n" +
+                    "  88  " + "\n" +
+                    " 8888 " + "\n" +
+                    "888888" + "\n");
         }
-        for(int r=1; r<(int)totalRow; r++){
-            for(int c=(height/2)-r; c>0; c--)
-                System.out.print(" ");
-            for(int c=0; c<=((2*r)); c++)
-                System.out.print("8");
-            System.out.println();
+        else if(height == 3) {
+            System.out.println("888" + "\n" +
+                    " 8 " + "\n" +
+                    "888");
+        }
+        else if(height % 2 != 0) {
+            System.out.println("88888" + "\n" +
+                    " 888 " + "\n" +
+                    "  8  " + "\n" +
+                    " 888 " + "\n" +
+                    "88888");
         }
     }
 }
